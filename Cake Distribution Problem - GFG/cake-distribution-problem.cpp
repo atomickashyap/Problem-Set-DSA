c408984f871a9c3f38ddc5aaaa1fd56ef7394c40
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-05-02 08:27:49 +0530

    Added solution - LeetHub

---
+++ Cake Distribution Problem - GFG/cake-distribution-problem.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    int maxSweetness(vector<int>& arr, int N, int K) {
+        int sum = 0;
+        for(int i=0;i<N;i++)
+        {
+            sum+=arr[i];
+        }
+        int l = 0;
+        int h = sum;
+        int ans = INT_MIN;
+        int mid;
+        while(l<=h)
+        {
+            mid = (l+h)>>1;
+            int ct = K+1;
+            int sm = 0;
+            int i=0;
+            while(i<N)
+            {
+                sm+=arr[i];
+                if(sm>=mid)
+                {
+                    ct--;
+                    sm=0;
+                }
+                i++;
+            }
+            if(ct<=0)
+            {
+                l = mid+1;
+                ans = max(ans,mid);
+            }
+            else{
+                h = mid-1;
+            }
+            
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> sweetness(n);
+        for (int i = 0; i < n; i++) {
+            cin >> sweetness[i];
+        }
+          Solution ob;
+          int res=ob.maxSweetness(sweetness, n, k);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
