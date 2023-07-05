2265711ccd15ca573f6c838fe3ea8647d8121bc2
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-07-05 18:17:18 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -6,36 +6,46 @@ using namespace std;
 // } Driver Code Ends
 
 int t[1002][1002];
-
 class Solution
 {   
     private:
     int recur(int W,int wt[],int val[],int n)
     {
-        if(W == 0|| n ==0)
+        for(int i = 0 ;i<n+1;i++)
         {
-            return 0;
+            for(int j = 0; j<W+1;j++)
+            {
+                if(i == 0|| j==0 )
+                {
+                    t[i][j] =0;
+                }
+            }
         }
         
-        if(t[n][W] != -1)
-            return t[n][W];
-        
-        if(wt[n-1] <= W)
+        for(int i = 1;i<n+1;i++)
         {
-            return t[n][W] = max(val[n-1]+ recur(W - wt[n-1], wt,val,n-1)
-                ,recur(W, wt,val,n-1));
+            for(int j = 1;j<W+1;j++)
+            {
+                if(wt[i-1] <= j)
+                {
+                    t[i][j] = max( val[i-1] + t[i-1][j-wt[i-1]]
+                        ,t[i-1][j] );
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
         }
-        else if(wt[n-1] > W){
-            return t[n][W] =  recur(W, wt,val,n-1);
-        }
+        
+        
+        return t[n][W];
+        
     }
     public:
-    //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-        memset(t,-1,sizeof(t));
+        // memset(t,-1,sizeof(t));
         return recur(W,wt,val,n);
-        // return 0;
     }
 };
 
