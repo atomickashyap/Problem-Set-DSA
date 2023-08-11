6b840aaaefac85790646b13f74687639d872b08b
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-08-11 14:16:03 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,39 +5,34 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    long long int count(int coins[], int n, int sum) {
+    long long n;
+    long long t[1001][1001];
+    long long solve(int idx,int sum,int coins[]){
+        if(sum==0){
+            return 1;
+        }
+        if(idx==n){
+            return 0;
+        }
+        if(t[idx][sum]!=-1){
+            return t[idx][sum];
+        }
+        if(sum<coins[idx]){
+            return t[idx][sum]=solve(idx+1,sum,coins);
+        }
+        
+        long long take=solve(idx,sum-coins[idx],coins);
+        long long skip=solve(idx+1,sum,coins);
+        
+        return t[idx][sum] =take+skip;
+        
+    }
+    long long int count(int coins[], int N, int sum) {
 
         // code here.
-        
-        long long t[n+1][sum+1];
-        for(int i = 0;i<n+1;i++)
-        {
-            for(int j = 0;j<sum+1;j++)
-            {
-                if(i==0)
-                    t[i][j] = 0;
-                if(j==0)
-                    t[i][j] = 1;
-                    
-            }
-        }
-        
-        for(int i = 1;i<n+1;i++)
-        {
-            for(int j = 1;j<sum+1;j++)
-            {
-                if(coins[i-1]<=j)
-                {
-                    t[i][j] = t[i][j-coins[i-1]] + t[i-1][j]; 
-                }
-                else
-                {
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-        
-        return t[n][sum];
+        n=N;
+        memset(t,-1,sizeof(t));
+        return solve(0,sum,coins);
     }
 };
 
