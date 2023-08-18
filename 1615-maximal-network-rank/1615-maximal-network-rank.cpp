6188d6aa0464d8ca7af39ac13eda4a3f3709ab14
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-08-18 21:30:03 +0530

    Time: 97 ms (52.51%), Space: 39 MB (37.23%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -10,8 +10,12 @@ public:
         int maxi=0;
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=0;j<n;j++)
             {
+                if(i==j)
+                {
+                    continue;
+                }
                 int ans=0;
                 ans+=adj[i].size();
                 ans+=adj[j].size();
