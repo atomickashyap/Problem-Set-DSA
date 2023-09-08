27fbfe0e648eff18261bc3f8cb196de30522c8aa
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-09-08 11:08:24 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -8,37 +8,41 @@ using namespace std;
 //User function template for C++
 
 class Solution{   
+private:
+    int mem(vector<int>arr,int sum,int n,vector<vector<bool>>&t)
+    {
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j =1;j<sum+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        // return t[n][sum];
+    }
 public:
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
         int n = arr.size();
-        bool t[n+1][sum+1];
-        for(int i = 0;i<n+1;i++)
+        vector<vector<bool>> t(n+1,vector<bool>(sum+1));
+        for(int i =0;i<n+1;i++)
         {
-            for(int j = 0;j<sum+1;j++)
+            for(int j= 0;j<sum+1;j++)
             {
-                if(i ==0)
+                if(i==0)
                     t[i][j] = false;
-                if(j ==0)
+                if(j==0)
                     t[i][j] =true;
             }
         }
-        
-        for(int i = 1;i<n+1;i++)
-        {
-            for(int j = 1;j<sum+1;j++)
-            {
-                if(arr[i-1] <=j)
-                {
-                     t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
-                }
-                else
-                    t[i][j] = t[i-1][j];
-            }
-        }
-        
+        mem(arr,sum,n,t);
         return t[n][sum];
-        
     }
 };
 
