f9751899eb6168aec6cd9e1bf2bfb442ed2df39e
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-09-08 11:34:53 +0530

    Time: 293 ms (46.62%), Space: 13.2 MB (64.37%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,31 @@
 class Solution {
-    
-private:
-    bool subset_sum(vector<int>& nums, int sum)
-    {
-        int n = nums.size();
-        bool t[n+1][sum+1];
+public:
+    bool canPartition(vector<int>& nums) {
         
-        for(int i = 0; i <n+1;i++)
+        int sum = 0;
+        int n = nums.size();
+        for(int i =0;i<n;i++)
         {
-            for(int j = 0;j<sum+1;j++)
-            {
-                if(i ==0 )
-                    t[i][j] = false;
-                if(j == 0)
-                    t[i][j] = true;
-            }
+            sum+=nums[i];
+        }
+        if((sum&1))
+        {
+            return false;
+        }
+            
+        sum = sum/2;
+        vector<vector<bool>> t(n+1,vector<bool> (sum+1,false));
+        
+        for(int i =0;i<n+1;i++)
+        {
+            t[i][0] =true;
         }
         
-        for(int i = 1; i <n+1;i++)
+        for(int i =1;i<n+1;i++)
         {
-            for(int j = 1;j<sum+1;j++)
+            for(int j =1;j<sum+1;j++)
             {
-                if(nums[i-1] <= j )
+                if(nums[i-1]<=j)
                 {
                     t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
                 }
@@ -31,18 +35,5 @@ private:
             }
         }
         return t[n][sum];
-    }
-public:
-    bool canPartition(vector<int>& nums) {
-        long long int sum = 0;
-        for(int i = 0;i<nums.size();i++)
-        {
-            sum+=nums[i];
-        }
-        
-        if(sum&1)
-            return false;
-        else
-            return subset_sum(nums,sum/2);
     }
 };
