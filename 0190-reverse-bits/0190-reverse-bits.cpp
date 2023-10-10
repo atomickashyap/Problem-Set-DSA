9964436a9c3a2fc878261b9bda982f65d8b6bc05
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-10-10 21:19:37 +0530

    Time: 4 ms (36.83%), Space: 6.5 MB (11.29%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        string ans = "";
-        while(n!=0){
-            ans+=(n%2+'0');
-            n/=2;
+        uint32_t res =0;
+        for(int i=0;i<32;i++)
+        {
+            uint32_t lsb = n&1;
+            uint32_t reverse = lsb<<(31-i);
+            res = res|reverse;
+            n = n>>1;
         }
-        int x = 32-ans.size();
-        while(x!=0){
-            ans+='0';x--;
-        }
-        int i=ans.size()-1;
-        int j=0;
-        uint32_t sum=0;
-        while(i>=0){
-            sum+=((ans[i]-'0')*pow(2,j++));
-            i--;
-        }
-        return sum;
+        return res;
     }
 };
