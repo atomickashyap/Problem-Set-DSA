ef8635627b4a51070c242cc6ee995b6cdd418103
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-11-10 13:19:38 +0530

    Time: 5 ms (28.54%), Space: 6.3 MB (34.36%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    int divide(long long dividend, long long divisor) {
+    int divide(int dividend, int divisor) {
         if(dividend == -2147483648 && divisor == -1) return 2147483647;
         
         if(dividend == -2147483648 && divisor == 1) {
             return -2147483648;
         }
-        int sign = ((dividend<0) ^ (divisor<0))? -1:1;//basically if any one is true(negative)
-        //then the sign value will be negative other wise it will be positive;
+        long long n = dividend, m = divisor;
+        long long sign  = (n<0 ^ m<0)? -1:1;
         
-        dividend = abs(dividend);
-        divisor = abs(divisor);
+        n = abs(n);
+        m = abs(m);
+        // dividend - quotient*divisor >=0;
+        long long q = 0;
+        long long t = 0;
         
-        // logic is basically how to represent a number in term of quotient and remainder
-        // Number = Divisor X  (Quotient) + Remainder;
-        // 10    =  3 X(3) + 1;
-        
-        long long quotient = 0, temp = 0;
-        
-        for(int i = 31;i>=0;i--)
+        for(int i =31;i>=0;i--)
         {
-            
-            if((temp+(divisor<<i)) <= dividend)
+            if(t+(m<<i)<=n)
             {
-                temp+=divisor<<i;
-                quotient += 1LL <<i;
+                t+=(m<<i);
+                q+=(1LL<<i);
             }
-            
         }
         
         
-        if(sign == -1)
-            quotient = -quotient;
         
-        return quotient;
-        
+        return int(q*sign);
     }
 };
