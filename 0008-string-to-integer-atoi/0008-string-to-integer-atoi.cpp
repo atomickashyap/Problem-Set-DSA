76815d2e511ab8bf2d795704b9de9b6959903d6a
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-11-10 10:14:58 +0530

    Time: 3 ms (57.67%), Space: 6.6 MB (64.21%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        if(s.length()==0)   return 0;
+        int n = s.length();
         int i =0;
-        while(i<s.size() && s[i]==' ')
+        while(i<n && s[i] == ' ')
         {
             i++;
         }
-        s = s.substr(i);
-        
-        int sign = +1;
-        long long ans = 0;
-        
-        if(s[0] == '-') sign = -1;
-        
-        int maxi = INT_MAX,mini = INT_MIN;
-        i = (s[0]=='-'||s[0]=='+')? 1:0;
-        while(i<s.length())
+        if(n==0)
         {
-            if(s[i]==' ' || !isdigit(s[i]))  return sign*ans;
-            
-            ans = ans*10 + s[i]-'0';
-            if(sign == -1 && -1*ans < mini) return mini;
-            if(sign == 1 && ans > maxi) return maxi;
+            return 0;
+        }
+        s = s.substr(i);
+        // cout<<s;
+        int sign = 1;
+        i = 0;
+        if(s[i] == '-')
+        {
+            sign=-1;
+        }
+        if(s[i] == '-' || s[i] == '+')
+        {
+            i = 1;
+        }
+        long long ans =  0;
+        int mini = INT_MIN,maxi = INT_MAX;
+        while(i < s.length())
+        {
+            if(s[i] == ' ' || !isdigit(s[i]))   return sign*ans;
+            ans = ans*10 + (s[i]-'0');
+            if(ans*sign < mini) return mini;
+            if(ans*sign > maxi) return maxi;
             i++;
-        }   
-        return int(sign*ans);
+        }
         
+        return int(ans*sign);
     }
 };
