4b1ac774323d69d633d7eca24cb5c1bf37c62d68
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-11-11 10:46:50 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (33.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int i=0, j=n-1;
-        while(i<=j)
+        
+        
+        int low = 0,high = nums.size()-1;
+        while(low<=high)
         {
-            //For rotated arrays (Case 1), 
-            if(nums[i]>=nums[j])
-            {
-                //check if the target matches at ith or jth
-                if(target==nums[i])
-                return i;
-                else if(target==nums[j])
-                return j;
-                //if not, iteratively increment i and decrement j.
-                else
-                i++;
-                j--;
-            }
-            else
-            {
-                //nums[i]<nums[j], array is sorted between ith and jth index
-                //apply binary search
-                int mid = i + (j - i) / 2;
-                if(nums[mid]<target)
-                    i=mid+1;
-                else if(nums[mid]>target)
-                    j=mid-1;
-                else
+            int mid = (low+high)/2;
+            if(nums[mid] == target)
                 return mid;
+            if(nums[low]<=nums[mid])
+            {
+                if(nums[low] <= target && target<=nums[mid])
+                {
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
+            else{
+                if(nums[mid] <= target && target<=nums[high])
+                {
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+            
         }
-        //if Element is not found return -1.
+        
+        
         return -1;
     }
 };
