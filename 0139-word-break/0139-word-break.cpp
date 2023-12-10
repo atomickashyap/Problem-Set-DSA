eca0713c2bba90ed15571d94c516518ee0d486f8
Aman Kashyap <76860442+AmanKas@users.noreply.github.com>
2023-12-10 21:17:54 +0530

    Time: 39 ms (7.29%), Space: 19.2 MB (8.88%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,39 +1,37 @@
-
 class Solution {
-private:
-    unordered_set<string>st;
+    unordered_set<string> st;
     std::vector<int> dp = std::vector<int>(301,-1);
-// int dp[301];
 private:
-    bool solver(string s, int idx)
+    bool solve(string s,int idx)
     {
-        if(idx == s.length())
+        if(s.length() == idx)
         {
             return true;
         }
-        if(dp[idx]!=-1) return dp[idx];
-        if(st.find(s)!=st.end())
-            return true;
-        for(int l = 1; l<=s.length();l++)
+        if(dp[idx] !=-1)
+            return dp[idx];
+        if(st.find(s) !=st.end())
+        {
+            return dp[idx] = true;
+        }
+        
+        for(int l = 1;l<s.length();l++)
         {
             string temp = s.substr(idx,l);
-            if(st.find(temp)!=st.end() && solver(s,idx+l))
+            if((st.find(temp)!=st.end()) && solve(s,idx+l))
             {
-                return dp[idx] = true;
+                return dp[idx]=true;
             }
         }
-        return dp[idx] = false;
+        return dp[idx]=false;
     }
+    
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        
-        // memset(dp,-1,sizeof(dp));        
         for(int i =0;i<wordDict.size();i++)
         {
             st.insert(wordDict[i]);
         }
-        
-        return solver(s,0);
-        
+        return solve(s,0);
     }
 };
